ba6581c11590cfcdca58368566e40dd3044c45cf
RedHammer <rassadin.rp@gmail.com>
2024-07-01 05:14:54 +0400

    Update games.html, index.html, and 2 more files...

--- observer.js
+++ observer.js
@@ -46,3 +46,11 @@ showBtn.addEventListener('click', () => {
     else { resizableDiv.style.display = 'block'; }
 
 });
+
+document.getElementById('game').addEventListener('mousemove', function(e) {
+    var w = window.innerWidth / 2;
+    var h = window.innerHeight / 2;
+    var x = (e.pageX - w) / 10;
+    var y = (e.pageY - h) / 10;
+    document.getElementById('object').style.transform = 'rotateX(' + y + 'deg) rotateY(' + x + 'deg)';
+  });
